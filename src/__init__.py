465dc9f7e4e357a4735d8e716b36257313d1dba5
Ibrahim.Badr <ch.ibrahimbadr@gmail.com>
2025-11-08 23:58:19 +0100

    feat: add WeatherDataModel class with validation and serialization methods

---
+++ src/__init__.py
@@ -0,0 +1,2 @@
+"""Weather Data ETL Visualization Package."""
+__version__ = "1.0.0"
---
+++ src/models/__init__.py
@@ -0,0 +1,4 @@
+"""Data models package."""
+from .weather_model import WeatherDataModel
+
+__all__ = ['WeatherDataModel']
